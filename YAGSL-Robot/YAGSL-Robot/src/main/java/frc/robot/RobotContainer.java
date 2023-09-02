315610103260064f5f003717140291b84fa1b76b
ary <cjansah11@gmail.com>
2023-09-01 22:17:42 -0400

    start swerve subsystem
    oh yeah yeah

--- YAGSL-Robot/YAGSL-Robot/src/main/java/frc/robot/RobotContainer.java
+++ YAGSL-Robot/YAGSL-Robot/src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,17 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.*;
+
+import frc.robot.Constants.*;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import java.io.File;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,6 +25,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), Constants.DriveConstants.driveDirectoryName));
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
--- YAGSL-Robot/YAGSL-Robot/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ YAGSL-Robot/YAGSL-Robot/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -6,12 +6,34 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import java.io.File;
+import java.io.IOException;
+
+import swervelib.SwerveController;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveControllerConfiguration;
+import swervelib.parser.SwerveDriveConfiguration;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
 public class SwerveSubsystem extends SubsystemBase {
   /** Creates a new SwerveSubsystem. */
-  public SwerveSubsystem() {}
+  SwerveDrive swerve;
+
+  public SwerveSubsystem(File swerveDirectory) {
+    File swerveJsonDirectory = swerveDirectory;
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+    try {
+      swerve = new SwerveParser(swerveJsonDirectory).createSwerveDrive();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    
   }
 }
