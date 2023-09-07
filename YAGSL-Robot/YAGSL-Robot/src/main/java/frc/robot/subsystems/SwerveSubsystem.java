b0f2007d465151d778200a33794db3ecfed0b278
ary <cjansah11@gmail.com>
2023-09-07 08:20:55 -0400

    complete swerve subsystem

--- YAGSL-Robot/YAGSL-Robot/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ YAGSL-Robot/YAGSL-Robot/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -17,6 +17,12 @@ import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class SwerveSubsystem extends SubsystemBase {
   /** Creates a new SwerveSubsystem. */
   SwerveDrive swerve;
@@ -31,6 +37,62 @@ public class SwerveSubsystem extends SubsystemBase {
       throw new RuntimeException(e);
     }
   }
+
+    public void drive(Translation2d translation, double theta, boolean isFieldOriented, boolean isOpenLoop) {
+      swerve.drive(translation, theta, isFieldOriented, isOpenLoop);
+    }
+
+    public Pose2d getPose() {
+      return swerve.getPose();
+    }
+
+    public void resetOdom(Pose2d pose) {
+      swerve.resetOdometry(pose);
+  }
+
+  public void setChassisSpeeds(ChassisSpeeds cs) {
+    swerve.setChassisSpeeds(cs);
+  }
+
+  public void addTrajectory(Trajectory traj) {
+    swerve.postTrajectory(traj);
+  }
+
+  public void zeroGyro() {
+    swerve.zeroGyro();
+  }
+
+  public void motorsBrake(boolean brake) {
+    swerve.setMotorIdleMode(brake);
+  }
+
+  public Rotation2d getHeading() {
+    return swerve.getYaw();
+  }
+
+  public ChassisSpeeds getTargetVelocities(double x, double y, Rotation2d theta) {
+    x = Math.pow(x, 3);
+    y = Math.pow(y, 3);
+    return swerve.getSwerveController().getTargetSpeeds(x, y, theta.getDegrees(), getHeading().getRadians());
+  }
+
+  public ChassisSpeeds getFieldRelativeVelocity() {
+    return swerve.getFieldVelocity();
+  }
+
+  public ChassisSpeeds getRobotRelativeVelocity() {
+    return swerve.getRobotVelocity();
+  }
+
+  public SwerveDriveConfiguration getSwerveConfigs() {
+    return swerve.swerveDriveConfiguration;
+  }
+
+  public void lockDrive() {
+    swerve.lockPose();
+  }
+
+
 
   @Override
   public void periodic() {
